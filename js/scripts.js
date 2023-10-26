227c8c46cf0c0c1320b5c16f314e50204563e877
Ocean <77823676+Ocean48@users.noreply.github.com>
2023-10-26 13:04:49 -0400

    responsive css

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// Mobile menu icon
+// var menuIcon = document.getElementById('menu_icon');
+// var menu = document.querySelector('.menu');
+
+// menuIcon.addEventListener('click', function() {
+//     menu.classList.toggle('active');
+// });
