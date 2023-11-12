16fd69b3a08f2718f962259f525d983daa932ad5
Ocean <77823676+Ocean48@users.noreply.github.com>
2023-11-12 18:22:12 -0500

    update

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,39 @@
 // menuIcon.addEventListener('click', function() {
 //     menu.classList.toggle('active');
 // });
+
+document.addEventListener("DOMContentLoaded", function () {
+    var skillBars = document.querySelectorAll(".skill-bar");
+
+    function isInViewport(element) {
+        var rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function updateSkillBars() {
+        skillBars.forEach(function (skillBar) {
+            if (isInViewport(skillBar) && !skillBar.classList.contains("active")) {
+                var percentage = skillBar.getAttribute("data-percentage");
+                var fill = skillBar.querySelector(".fill");
+                var percentageText = skillBar.querySelector(".percentage");
+
+                fill.style.width = percentage + "%";
+                percentageText.textContent = percentage + "%";
+
+                skillBar.classList.add("active");
+            }
+        });
+    }
+
+    // Update skill bars on page load
+    updateSkillBars();
+
+    // Update skill bars on scroll
+    window.addEventListener("scroll", updateSkillBars);
+});
+
